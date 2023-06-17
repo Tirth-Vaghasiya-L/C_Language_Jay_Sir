e4fd32c7e0fa4356b7ab4c8604ae4f86c95fee60
Tirth Vaghasiya <114156386+Tirth-Vaghasiya-L@users.noreply.github.com>
2023-06-17 18:23:07 +0530

    Add files via upload

---
+++ CH-1-1.C
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+
+main()
+{
+	clrscr();
+
+	printf("Name\t:-  Tirth\n");
+	printf("Age\t:-  14\n");
+	printf("STD\t:-  9\n");
+	printf("Course  :-  Flutter\n");
+	printf("School\t:-  G.V.V.T.C\n");
+	printf("Hobby\t:-  Coding\n");
+	printf("Address :-  Shivan Avenue\n");
+	printf("Song\t:- Suzume No Tojimare");
+
+	getch();
+}
