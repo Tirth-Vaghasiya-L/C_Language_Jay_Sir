9d8b1ae82d80fa77014d9cbd9c042280dafb656a
Tirth Vaghasiya <114156386+Tirth-Vaghasiya-L@users.noreply.github.com>
2023-06-22 18:42:31 +0530

    Update CH-9-2-1.c

--- CH-9-2-1.c
+++ CH-9-2-1.c
@@ -11,38 +11,6 @@ main()
 	
 	leng = strlen(pass);
 	
-	/*
-			for(i=0;i<length;i++)
-			{
-				if(pass[i]>=65 && pass[i]<=90)
-				{
-					u++;
-				}
-				else if(pass[i]>=97 && pass[i]<=122)
-				{
-					l++;
-				}
-				else if(pass[i]>=48 && pass[i]<=57)
-				{
-					d++;
-				}
-				
-				else
-				{
-					s++;
-				}
-			}
-			
-		if(u>0 && d>0 && s>0 && l>0 &&length>=6)
-		{
-			printf("your password is strong..");
-		}
-		else
-		{
-			printf("your password is not strong..");
-		}	
-	*/
-	
 	for(i=0;i<leng;i++)
 	{
 		if(pass[i] >= 65 && pass[i] <= 90 || pass[i] >= 97 && pass[i] <= 122)
